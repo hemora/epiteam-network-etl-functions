6b727b631480c518a2fdd473c710cd6c0d0c0e09
hmora <hmora@ciencias.unam.mx>
2023-06-12 17:00:34 +0000

    DuckSession can now be initialized with a storage file

--- src/utils/duckaccess.py
+++ src/utils/duckaccess.py
@@ -3,8 +3,11 @@ import duckdb
 class DuckSession:
     """
     """
-    def __init__(self) -> None:
-        self.__duck = duckdb.connect()
+    def __init__(self, storage_path: str = None) -> None:
+        if storage_path is None:
+            self.__duck = duckdb.connect()
+        else:
+            self.__duck = duckdb.connect(storage_path)
 
     def __enter__(self):
         return self.__duck
